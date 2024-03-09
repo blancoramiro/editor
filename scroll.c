10cd04cb9629dea0d9f3448d784e9fe5f2c90da3
Ramiro Blanco <r@miroblan.co>
2024-03-09 01:10:41 -0300

    some clean up

--- scroll.c
+++ scroll.c
@@ -5,7 +5,7 @@
  *
  */
 
-#include <stdio.h>
+#include <stdio.h> //this needs to go
 
 #define GLFW_INCLUDE_ES3
 #include <GLFW/glfw3.h>
@@ -26,7 +26,11 @@ void scroll_bar_update()
 	{
 		scroll_bar.top = window_size.height - window_size.height / lines_count * lines_scroll;
 		scroll_bar.bottom = (lines_count - (int) grid.height - lines_scroll) * (window_size.height / lines_count);
-		printf("ls: %u lc: %u bottom lc: %u chunk: %f top: %f bottom: %f\n", lines_scroll, lines_count, lines_count - (int) grid.height - lines_scroll, window_size.height / lines_count, scroll_bar.top, scroll_bar.bottom);
+		printf("ls: %u lc: %u bottom lc: %u chunk: %f top: %f bottom: %f\n",
+				lines_scroll,
+				lines_count,
+				lines_count - (int) grid.height - lines_scroll,
+				window_size.height / lines_count, scroll_bar.top, scroll_bar.bottom);
 		
 	}
 	glUniform2fv(scroll_bar_location, 1, (GLfloat*) &scroll_bar);
@@ -36,11 +40,6 @@ void scroll_bar_update()
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	cursor_reset();
-//	printf("%u %f\n", lines_scroll, yoffset);
 	lines_scroll += yoffset < 0 ? grid.height + lines_scroll < lines_count ? 1 : 0 : lines_scroll > 0 ? -1 : 0;
-//	printf("%u %f\n", lines_scroll, yoffset);
 	get_top_paragraph_scroll();
-//	update_chars_tex();
-//	load_selection_tex();
-//	scroll_bar_update();
 }
