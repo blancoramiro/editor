2bcfcf57a7995656a9d8cc728ed33656b2ca90c0
Ramiro Blanco <r@miroblan.co>
2024-03-09 14:44:48 -0300

    emcc updates; window resize web

--- editor_extra_post.js
+++ editor_extra_post.js
@@ -1,13 +1,20 @@
 
-function call_draw() {
+function call_draw()
+{
 	Module.ccall('draw', // name of C function
 	null, // return type
 	[], // argument types
 	[]);
-    }
+}
 
+function canvas_resize()
+{
+	canvas.width = document.body.offsetWidth;
+	canvas.height = 300; 
+}
 
-window.addEventListener('paste', function (e) {
+window.addEventListener('paste', function (e)
+{
 	if(e.clipboardData != false) {
 		//console.log(e.clipboardData.getData('Text'));
 		let chars = e.clipboardData.getData('Text').split('');
@@ -23,16 +30,17 @@ window.addEventListener('paste', function (e) {
 	}
 }, false);
 
-window.addEventListener("keypress", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false});
+window.addEventListener("keypress", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false });
 
-window.addEventListener("keydown", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false});
+window.addEventListener("keydown", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false });
 
-window.addEventListener("keyup", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false});
+window.addEventListener("keyup", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false });
 
-window.addEventListener("click", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false});
+window.addEventListener("click", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false });
 
-window.addEventListener("mousemove", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false});
+window.addEventListener("mousemove", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false });
 
-window.addEventListener("scroll", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false});
+window.addEventListener("scroll", function(/*e*/) { call_draw(); /*e.preventDefault();*/ return false });
 
-window.addEventListener("resize", function(/*e*/) { canvas.width = document.body.offsetWidth; canvas.height = document.body.offsetHeight; return false});
+window.addEventListener("resize", function(/*e*/) { canvas_resize(); return false });
+
